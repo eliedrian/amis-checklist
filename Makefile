0bc296d0d3dbdaf0fc5d79a3849d6a4c73a29422
Eli Tan <5410435+eliedrian@users.noreply.github.com>
2025-08-07 23:42:04 +0800

    ingest: write classes and courses ingest

--- Makefile
+++ Makefile
@@ -6,6 +6,8 @@ FILTER=$(FILTERS_DIR)/filter.jq
 USERS_FILTER=$(FILTERS_DIR)/users_filter.jq
 STUDENTS_FILTER=$(FILTERS_DIR)/students_filter.jq
 STUDENTS_JOIN_FILTER=$(FILTERS_DIR)/students_join_filter.jq
+CLASSES_FILTER=$(FILTERS_DIR)/classes_filter.jq
+COURSES_FILTER=$(FILTERS_DIR)/courses_filter.jq
 
 RAW_STUDENT_GRADES=$(wildcard $(DATA_DIR)/student-grades*.json)
 STUDENT_IDS=student_ids.txt
@@ -19,18 +21,24 @@ GRADES_JSON=$(ODIR)/grades.json
 _USERS_JSON=$(ODIR)/_users.jsonl
 _STUDENTS_JSON=$(ODIR)/_students.jsonl
 STUDENTS_JSON=$(ODIR)/students.json
+CLASSES_JSON=$(ODIR)/classes.json
+COURSES_JSON=$(ODIR)/courses.json
 
 INIT_SQL=$(ODIR)/init.sql
 
 RAW_USERS=$(wildcard $(DATA_DIR)/users*.json)
 RAW_STUDENTS=$(wildcard $(DATA_DIR)/students*.json)
+RAW_CLASSES=$(wildcard $(DATA_DIR)/classes*.json)
+RAW_COURSES=$(wildcard $(DATA_DIR)/courses*.json)
 
 INGEST_MARKER_STUDENTS=$(ODIR)/.ingested_students
 INGEST_MARKER_GRADES=$(ODIR)/.ingested_grades
+INGEST_MARKER_CLASSES=$(ODIR)/.ingested_classes
+INGEST_MARKER_COURSES=$(ODIR)/.ingested_courses
 
 TARGETS=database collectgrades collectstudents ingest
 
-.phony: all clean collectgrades collectstudents query ingeststudents ingest ingestgrades
+.phony: all clean collectgrades collectstudents query ingeststudents ingest ingestgrades ingestclasses ingestcourses
 
 all: $(TARGETS)
 
@@ -44,11 +52,23 @@ $(INIT_SQL):
 query: $(INIT_SQL)
 	sqlite3 -table -header -init $<
 
-ingest: ingeststudents ingestgrades
+ingest: ingeststudents ingestgrades ingestclasses ingestcourses
 
 ingeststudents: $(INGEST_MARKER_STUDENTS)
 
+ingestcourses: $(INGEST_MARKER_COURSES)
+
 ingestgrades: $(INGEST_MARKER_GRADES)
+
+ingestclasses: $(INGEST_MARKER_CLASSES)
+
+$(INGEST_MARKER_COURSES): $(COURSES_JSON) | $(BRONZE_DB_PATH)
+	python ingest.py --db $(BRONZE_DB_PATH) courses $(COURSES_JSON)
+	touch $@
+
+$(INGEST_MARKER_CLASSES): $(CLASSES_JSON) | $(BRONZE_DB_PATH)
+	python ingest.py --db $(BRONZE_DB_PATH) classes $(CLASSES_JSON)
+	touch $@
 
 $(INGEST_MARKER_STUDENTS): $(STUDENTS_JSON) | $(BRONZE_DB_PATH)
 	python ingest.py --db $(BRONZE_DB_PATH) students $(STUDENTS_JSON)
@@ -66,6 +86,12 @@ database: $(SILVER_DB_PATH) $(BRONZE_DB_PATH)
 collectstudents: $(STUDENTS_JSON)
 
 collectgrades: $(GRADES_JSON)
+
+$(COURSES_JSON): $(COURSES_FILTER)
+	jq -f $(COURSES_FILTER) -s $(RAW_COURSES) > $@
+
+$(CLASSES_JSON): $(CLASSES_FILTER)
+	jq -f $(CLASSES_FILTER) -s $(RAW_CLASSES) > $@
 
 $(STUDENTS_JSON): $(_USERS_JSON) $(_STUDENTS_JSON) $(STUDENTS_JOIN_FILTER)
 	jq -n --slurpfile users $(_USERS_JSON) --slurpfile students $(_STUDENTS_JSON) -f $(STUDENTS_JOIN_FILTER) > $@
--- ingest.py
+++ ingest.py
@@ -4,7 +4,7 @@ import json
 
 parser = argparse.ArgumentParser('ingest.py')
 parser.add_argument('--db', help='Path to an sqlite3 database.')
-parser.add_argument('type', choices=['students', 'grades'], help='Type of data to ingest.')
+parser.add_argument('type', choices=['students', 'grades', 'classes', 'courses'], help='Type of data to ingest.')
 parser.add_argument('source', help='JSON data source.')
 args = parser.parse_args()
 
@@ -21,6 +21,28 @@ def grade_tuple(g):
             g['unit_taken'], g['course_id'], g['grade'], g['college'],
             g['grade_type'], g['status'], g['remarks'], g['course_code'],
             g['student_name'])
+
+def course_tuple(c):
+    return ( c['course_id'], c['sais_course_id'], c['title'], c['type'],
+            c['description'], c['course_code'], c['sem_offered'], c['career'],
+            int(c['units']), c['is_repeatable'], c['is_active'], c['campus'],
+            c['equivalent'], c['is_multiple_enrollment'], c['subject'],
+            c['course_number'], c['contact_hours'], c['grading'], c['tm_id'],
+            c['acad_org'], c['acad_group'], c['created_at'], c['updated_at'],
+            c['status'], c['is_academic'], c['course_code_title'],
+            json.dumps(c['teaching_model']), json.dumps(c['requisites']))
+
+def class_tuple(c):
+    return (c['id'], c['course_code'], c['course_id'], c['term_id'],
+            c['parent_class_id'], c['type'], c['date'], c['section'],
+            c['time'], c['start_time'], c['end_time'], c['credit'],
+            c['tm_id'], c['facility_id'], c['max_class_size'],
+            c['reserved_count'], c['prerog_count'], c['active_class_size'],
+            c['total_enlistment'], c['activity'], c['acad_org'], c['acad_group'],
+            c['assoc'], c['class_nbr'], c['class_status'], c['consent_type'],
+            c['hide_faculty'], c['is_partial_posting'], c['is_parent_class'],
+            json.dumps(c['faculty_grades_assignments']), json.dumps(c['faculties']),
+            json.dumps(c['class_dates']))
 
 data = None
 
@@ -48,6 +70,31 @@ match args.type:
                 grade, college, grade_type, status, remarks, course_code, student_name) \
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
                         list(map(grade_tuple, data)))
+
+    case 'classes':
+        cur.executemany('INSERT OR IGNORE INTO Classes \
+                (id, course_code, course_id, term_id, parent_class_id, \
+                type, date, section, time, start_time, end_time, credit, \
+                tm_id, facility_id, max_class_size, reserved_count, \
+                prerog_count, active_class_size, total_enlistment, activity, \
+                acad_org, acad_group, assoc, class_nbr, class_status, \
+                consent_type, hide_faculty, is_partial_posting, is_parent_class, \
+                faculty_grades_assignments, faculties, class_dates) \
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, \
+                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                       list(map(class_tuple, data)))
+
+    case 'courses':
+        query = """
+INSERT OR IGNORE INTO Courses (
+    course_id, sais_course_id, title, type, description, course_code,
+    sem_offered, career, units, is_repeatable, is_active, campus, equivalent,
+    is_multiple_enrollment, subject, course_number, contact_hours, grading,
+    tm_id, acad_org, acad_group, created_at, updated_at, status, is_academic,
+    course_code_title, teaching_model, requisites)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """
+        cur.executemany(query, list(map(course_tuple, data)))
 
 con.commit()
 con.close()
